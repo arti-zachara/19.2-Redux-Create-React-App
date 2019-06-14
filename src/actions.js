7c645f194ec21160c63dca4590baca53f3524c90
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-14 10:16:11 +0200

    Add exports to actions

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,13 @@
 import uuid from "uuid";
 
-const ADD_COMMENT = "ADD_COMMENT",
-  REMOVE_COMMENT = "REMOVE_COMMENT",
-  EDIT_COMMENT = "EDIT_COMMENT",
-  THUMB_UP_COMMENT = "THUMB_UP_COMMENT",
-  THUMB_DOWN_COMMENT = "THUMB_DOWN_COMMENT";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const THUMB_UP_COMMENT = "THUMB_UP_COMMENT";
+export const THUMB_DOWN_COMMENT = "THUMB_DOWN_COMMENT";
 
 // ------ action creators --------------------
-function addComment(text) {
+export function addComment(text) {
   return {
     type: ADD_COMMENT,
     text,
@@ -15,14 +15,14 @@ function addComment(text) {
   };
 }
 
-function removeComment(id) {
+export function removeComment(id) {
   return {
     type: REMOVE_COMMENT,
     id
   };
 }
 
-function editComment(id, text) {
+export function editComment(id, text) {
   return {
     type: EDIT_COMMENT,
     text,
@@ -30,14 +30,14 @@ function editComment(id, text) {
   };
 }
 
-function thumbUpComment(id) {
+export function thumbUpComment(id) {
   return {
     type: THUMB_UP_COMMENT,
     id
   };
 }
 
-function thumbDownComment(id) {
+export function thumbDownComment(id) {
   return {
     type: THUMB_DOWN_COMMENT,
     id
