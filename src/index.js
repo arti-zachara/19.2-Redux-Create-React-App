c19863aaad0f6547a84133fa02e8a89476514a75
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-15 14:09:44 +0200

    Order imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-
 // ------------ inside-app imports ----------------------------------
 import * as serviceWorker from "./serviceWorker";
-import { reducer } from "./reducer";
 import DevTools from "./DevTools";
+import { reducer } from "./reducer";
 import App from "./App";
 import { addComment } from "./actions";
 import "./index.css";
