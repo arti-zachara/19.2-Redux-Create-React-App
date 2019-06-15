fcba8f0b54eb8055f220200586eaae431217da74
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-15 09:53:51 +0200

    Remove unnecessary files and import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore } from "redux";
 import { reducer } from "./reducer";
 import { addComment } from "./actions";
 import DevTools from "./DevTools";
